6e907f2435f9ade9cda41398f08e112b58375da9
mnguyen <mnguyen2606@gmail.com>
2020-11-18 00:56:23 +0700

    1470. Shuffle the Array

--- 1470. Shuffle the Array.py
+++ 1470. Shuffle the Array.py
@@ -1,15 +1,16 @@
 """
-Given an array of integers nums.
-A pair (i,j) is called good if nums[i] == nums[j] and i < j.
-Return the number of good pairs.
+Given the array nums consisting of 2n elements in the form
+[x1,x2,...,xn,y1,y2,...,yn].
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 """
 
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        numberOfPairs = 0
         
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] == nums[j]:
-                    numberOfPairs += 1
-        return numberOfPairs
+        shuffleList = []
+        
+        for i in range(n):
+            shuffleList.append(nums[i])
+            shuffleList.append(nums[i + n])
+        
+        return shuffleList
